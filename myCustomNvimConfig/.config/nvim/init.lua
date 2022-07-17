3b42550f74e40c1eea65003fef5922973ddfe3bf
Jack <jtrus93@gmail.com>
2022-07-17 18:23:03 -0400

    ^.^

--- myCustomNvimConfig/.config/nvim/init.lua
+++ myCustomNvimConfig/.config/nvim/init.lua
@@ -1,5 +1,7 @@
 require "plugins"
 require "plugins/configs/cmp"
 require "plugins/configs/lspconfig"
+require "plugins/configs/nvim-treesitter"
+require "plugins/configs/telescope"
 
 vim.api.nvim_command('colorscheme gruvbox')
