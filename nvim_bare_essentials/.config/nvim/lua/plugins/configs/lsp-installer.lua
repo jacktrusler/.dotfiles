7739cc5384f22eb604b5a7fdda6f50d7cf30b33a
Jack <jtrus93@gmail.com>
2022-07-18 22:18:07 -0400

    .....

--- nvim_bare_essentials/.config/nvim/lua/plugins/configs/lsp-installer.lua
+++ nvim_bare_essentials/.config/nvim/lua/plugins/configs/lsp-installer.lua
@@ -4,15 +4,69 @@ if not present then
    return
 end
 
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local vim_version = vim.version()
+
+   if vim_version.minor > 7 then
+      -- nightly
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+   else
+      -- stable
+      client.resolved_capabilities.document_formatting = false
+      client.resolved_capabilities.document_range_formatting = false
+   end
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
 -- only for initial setup, all other servers go in lspconfig.lua
-local server_is_found, server = lsp_installer.get_server('sumneko_lua')
+local server_is_found, lua = lsp_installer.get_server('sumneko_lua')
 
 if server_is_found then
-   if not server:is_installed() then
+   if not lua:is_installed() then
       print("installing " .. 'sumneko_lua')
-      server:install()
+      lua:install()
    end
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+lsp_installer.on_server_ready(function(server)
+
+   local default_opts = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+
+      settings = {
+         Lua = {
+            diagnostics = {
+               globals = { "vim" },
+            },
+         },
+      },
+   }
+   server:setup(default_opts)
+end)
 
 lsp_installer.setup({
     -- automatically detect which servers to install (based on which servers are set up via lspconfig)
