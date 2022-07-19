ed18edac7e2e8d192dc3e765575134866d14b63d
Jack <jtrus93@gmail.com>
2022-07-18 20:23:59 -0400

    <($.$<)

--- nvim_bare_essentials/.config/nvim/lua/plugins/init.lua
+++ nvim_bare_essentials/.config/nvim/lua/plugins/init.lua
@@ -3,7 +3,10 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
    Packer_Bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-   print "Installing packer: close and reopen Neovim..."
+   print "---------------------------------------------------------"
+   print "Installing packer..."
+   print "After, install, close and reopen Neovim to load configs!"
+   print "---------------------------------------------------------"
    vim.cmd [[packadd packer.nvim]]
 end
 
