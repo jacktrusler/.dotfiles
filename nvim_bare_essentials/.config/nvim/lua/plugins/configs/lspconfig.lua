f5e84615f4f055418070b6dd68cd5527b37e3ef4
Jack <jtrus93@gmail.com>
2022-07-18 20:42:28 -0400

    gamer alert

--- nvim_bare_essentials/.config/nvim/lua/plugins/configs/lspconfig.lua
+++ nvim_bare_essentials/.config/nvim/lua/plugins/configs/lspconfig.lua
@@ -59,14 +59,3 @@ lspconfig.sumneko_lua.setup {
       },
    },
 }
-
-require("nvim-lsp-installer").setup({
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
-    }
-})
