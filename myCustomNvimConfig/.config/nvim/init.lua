0b8e556909d759c51eeb5d8179266977e7d706c3
jacktrusler <89369559+jacktrusler@users.noreply.github.com>
2022-07-22 00:31:20 -0400

    Delete myCustomNvimConfig/.config/nvim directory

--- myCustomNvimConfig/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require "plugins"
-require "plugins/configs/cmp"
-require "plugins/configs/lspconfig"
-require "plugins/configs/nvim-treesitter"
-require "plugins/configs/telescope"
-
-vim.api.nvim_command('colorscheme gruvbox')
